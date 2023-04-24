e8788dfeb9ed64607b7973208cc2686e2a1029a8
Lich0116 <lichieh_chang@innodisk.com>
2023-04-24 13:25:59 +0800

    feat : application rename.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from apps.BasicClassification import BasicClassification
 from apps.BasicObjectDetection import BasicObjectDetection  
-from apps.Tracking import Tracking , event_handle , app_common_handle
-from apps.CountingArea import CountingArea , event_handle , app_common_handle
-from apps.Direction import Direction , event_handle , app_common_handle
+from apps.TrackingZone import TrackingZone , event_handle , app_common_handle
+from apps.DetectionZone import DetectionZone , event_handle , app_common_handle
+from apps.MovementZone import MovementZone , event_handle , app_common_handle
