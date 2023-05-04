40e3e0b842952ac2fb7d769d22d6f31c4f57bacd
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-04 14:56:01 +0800

    chore: add module

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,9 +5,9 @@
 
 # __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from .encode import gen_uuid, load_json, json_to_str, load_db_json, check_json, get_pure_jsonify
+from .encode import gen_uid, load_json, json_to_str, load_db_json, check_json, get_pure_jsonify
 from .network import get_mac_address, get_address
-
+ 
 from . import (
     encode,
     network
