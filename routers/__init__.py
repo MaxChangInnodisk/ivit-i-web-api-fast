7d343d5e608924038b22bb45b307fdb625fc5b5a
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-04 14:55:13 +0800

    feat: add icap ad proc router

--- routers/__init__.py
+++ routers/__init__.py
@@ -10,11 +10,15 @@ from .source import source_router
 from .model import model_router
 from .app import app_router
 from .device import device_router
+from .icap import icap_router
+from .process import proc_router
 
 routers = [
     task_router,
     source_router,
     model_router,
     app_router,
-    device_router
+    device_router,
+    icap_router,
+    proc_router
 ]
