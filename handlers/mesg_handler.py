b333fc6907ad4fe81dc5ac545a2a90f4f29cda7f
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-09 11:14:22 +0800

    feat: add ws_msg to handle message from websocket

--- handlers/mesg_handler.py
+++ handlers/mesg_handler.py
@@ -3,6 +3,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+from typing import Union, Literal
 import json
 import logging as log
 from fastapi.responses import Response
@@ -14,6 +15,10 @@ K_MESG  = "message"
 K_CODE  = "status_code"
 K_DATA  = "data"
 K_TYPE  = "type"
+
+# WebSocket
+K_UID = "UID"
+K_ERR = "ERROR"
 
 def json_exception(content) -> dict:
     """ Return a iVIT Exception with JSON format """
@@ -27,6 +32,33 @@ def json_exception(content) -> dict:
         K_MESG: err_detail if isinstance(err_detail, str) else json.dumps(err_detail),
         K_TYPE: err_type 
     }
+
+
+def ws_msg(content:Union[str, dict], type:Literal["UID","ERROR","TEMP"]) -> dict:
+    """ Web Socket response handler """
+
+    # Define Basic Format
+    ret = {
+        K_DATA: {},
+        K_MESG: "",
+        K_TYPE: type
+    }
+
+    # If is Exception
+    if isinstance(content, Exception):
+        log.exception(content)
+        ret[K_TYPE] = K_ERR
+        ret[K_MESG] = json_exception(content=content)[K_MESG]
+        
+    # If not Exception, check input content is String or Object
+    elif isinstance(content, str):
+        ret[K_MESG] = content
+
+    else:
+        ret[K_DATA] = content
+    
+    return ret
+        
 
 def http_msg(content, status_code:int=200, media_type:str="application/json"):
     """ HTTP response handler """
