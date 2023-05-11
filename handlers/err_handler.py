cd9377b7d135453d77eb6f28013bb121a80f656b
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-11 01:37:27 -0700

    feat: add er_handler

---
+++ handlers/err_handler.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2023 Innodisk Corporation
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+class ErrorWrapper(Exception):
+    def __init__(self, message:str) -> None:
+        self.message = message
+
+
+class InvalidError(ErrorWrapper):
+    pass
+
+
+class InvalidUidError(ErrorWrapper):
+    pass
