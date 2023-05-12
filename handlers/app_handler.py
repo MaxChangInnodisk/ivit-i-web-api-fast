3ab931dd74b0f87b174c6a0cfc059ac5de9c0a7f
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-12 02:43:59 -0700

    chore: add more annotation

--- handlers/app_handler.py
+++ handlers/app_handler.py
@@ -5,22 +5,38 @@
 
 import sys, os, time, re
 import logging as log
+from typing import Union
 
 # Import iVIT-I 
 from ..common import SERV_CONF, RT_CONF
 
 from .db_handler import select_data, parse_app_data
-from .ivit_handler import iAPP_HANDLER
+from .ivit_handler import iAPP_HANDLER, iAPP_CLS, iAPP_OBJ, iAPP_SEG
 
 
-def init_db_app():
+def init_db_app() -> None:
+    """Add iAPP_HANDLER into RT_CONF 
+    
+    Workflow:
+    1. Initialize iAPP_HANDLER
+    2. Register SERV_CONF['APP_DIR']
+    3. Add hanlder into RT_CONF with 'iAPP' key 
+    """
     app_handler = iAPP_HANDLER()
     app_handler.register_from_folder(app_folder=SERV_CONF["APP_DIR"])
     RT_CONF.update({"IAPP": app_handler})
 
 
-def create_app(app_uid:str, label_path:str):
-    """ Create execute application """
+def create_app(app_uid:str, label_path:str) -> Union[iAPP_CLS, iAPP_OBJ, iAPP_SEG]:
+    """Create execute application
+
+    Args:
+        app_uid (str): the applicatoin uid
+        label_path (str): the path to label file
+
+    Returns:
+        Union[iAPP_CLS, iAPP_OBJ, iAPP_SEG]: return application which inherit with iAPP_<TYPE>
+    """
     
     # Application Information
     app_data = select_data( 
@@ -31,7 +47,7 @@ def create_app(app_uid:str, label_path:str):
     
     # Instance App Object
     app = RT_CONF['IAPP'].get_app(app_info['name'])(
-        params = app_info['app_setting'], 
+        params = app_info['app_setting'],
         label = label_path
     )
     
