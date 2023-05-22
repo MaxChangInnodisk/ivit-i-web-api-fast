3f299b319299374580fdfc8dcaef632f85135845
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-22 17:20:07 +0800

    feat: add sciprt for adding apps submodule

---
+++ tools/add-app-submodule.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 2023 Innodisk Corporation
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+#!/bin/bash
+
+git submodule add -b r1.1 https://github.com/InnoIPA/ivit-i-app.git apps
+git commit -m "add submodule: ivit-i-app "
