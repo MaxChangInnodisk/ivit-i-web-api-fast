2b64d4bae00982c03d8426ef2073cbd90f8a065d
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-31 10:26:11 +0800

    feat: add utils to convert nvidia model

--- samples/utils.py
+++ samples/utils.py
@@ -8,6 +8,8 @@ import sys
 import time
 import zipfile
 import gdown
+import subprocess as sp
+import logging as log
 
 try:
     from ..common import SERV_CONF
@@ -45,7 +47,40 @@ def download_model(file_name, file_url):
     file_path = os.path.join(SERV_CONF["MODEL_DIR"], file_name)
     download_file(file_path, file_url)
     extract_file(file_path)
+    model_folder_path = os.path.splitext(file_path)[0]
+    if SERV_CONF['FRAMEWORK']=='tensorrt':
+        for file in os.listdir(model_folder_path):
+            if not os.path.splitext(file)[1] in [ '.onnx', '.weights', '.wts']:
+                continue
+            convert_model(os.path.join(model_folder_path, file))
 
 def download_data(file_name, file_url):
     file_path = os.path.join(SERV_CONF["DATA_DIR"], file_name)
     download_file(file_path, file_url)
+
+def convert_model(model_path:str):
+    
+    model_name, model_ext = os.path.splitext(model_path)
+
+    # Check
+    if model_ext == '':
+        raise NameError(f'Unexpected model path ({model_path}), the extension should be ".onnx", ".wts", ".weights"')
+
+    # Combine tensorrt engine path
+    trg_model_path = model_name + '.trt'
+
+    # Get correct command line
+    if '.onnx' in model_path:
+        # Classification
+        cmd = [ 
+            "/usr/src/tensorrt/bin/trtexec", 
+            f"--onnx={model_path}", 
+            f"--saveEngine={os.path.realpath(trg_model_path)}" ]
+    else:
+        cmd = [ "yolo-converter", f"{model_name}" ]
+
+    log.warning(f"Run Convert Process: {' '.join(cmd)}")    
+    flag = sp.run(args=cmd)
+    log.warning("Converted !")
+
+    return trg_model_path
