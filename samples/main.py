778dde08c65f8e8d4c52ce7639e2e6036483d6a0
MaxChangInnodisk <max_chang@innodisk.com>
2023-05-31 10:25:51 +0800

    feat: add nvidia_sample

--- samples/main.py
+++ samples/main.py
@@ -7,13 +7,17 @@
 from .intel_sample import init_intel_samples
 from .xlnx_sample import init_xlnx_samples
 from .hailo_sample import init_hailo_samples
+from .nvidia_sample import init_nvidia_samples
+
+
 def init_samples(framework: str):
     """ Initialize Samples into Database """
 
     sample_table = {
         'openvino': init_intel_samples,
         'vitis-ai': init_xlnx_samples,
-        'hailort': init_hailo_samples
+        'hailort': init_hailo_samples,
+        'tensorrt': init_nvidia_samples
     }
     func = sample_table.get(framework.strip().lower(), None) 
     
