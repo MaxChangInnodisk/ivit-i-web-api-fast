07c305449dda2f3f6ce6e73bc344d7f518545c2f
MaxChangInnodisk <max_chang@innodisk.com>
2023-06-13 17:52:46 +0800

    feat: add download files and change user permission

--- routers/system.py
+++ routers/system.py
@@ -7,7 +7,7 @@
 import json, threading, time, sys, os, re
 import logging as log
 from fastapi import APIRouter,status
-from fastapi.responses import Response
+from fastapi.responses import Response, FileResponse
 from typing import Optional, Dict, List
 from pydantic import BaseModel
 
@@ -24,6 +24,7 @@ except:
 # Router
 sys_router = APIRouter(tags=["system"])
 
+
 @sys_router.get('/v4l2')
 def get_v4l2_device_list():
     try:
@@ -33,3 +34,22 @@ def get_v4l2_device_list():
     
     except Exception as e:
         return http_msg(content=e, status_code=500)
+
+
+@sys_router.get("/files/{file_path}")
+def download_file(file_path):
+    try:
+        file_path = os.path.join( SERV_CONF["EXPORT_DIR"], file_path)
+
+        log.info('Start to download file: {}'.format(file_path))
+        return FileResponse(path=file_path, filename=os.path.basename(file_path))
+    except Exception as e:
+        return http_msg(content=e, status_code=500)
+
+
+@sys_router.get("/user/permit")
+def change_user_permission():
+    import subprocess as sb
+    sb.run("chown 1000:1000 -R .", shell=True)
+    return http_msg("done")
+
