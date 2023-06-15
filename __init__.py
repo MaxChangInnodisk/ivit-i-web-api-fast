f407e3a79cd692bbbaa96199896d0a8be72109a6
MaxChangInnodisk <max_chang@innodisk.com>
2023-06-15 11:10:58 +0800

    feat: remove unused module

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from apps.Basic_Classification import Basic_Classification
 from apps.Basic_Object_Detection import Basic_Object_Detection  
-from apps.Tracking_Zone import Tracking_Zone , event_handle , app_common_handle
-from apps.Detection_Zone import Detection_Zone , event_handle , app_common_handle
-from apps.Movement_Zone import Movement_Zone , event_handle , app_common_handle
+from apps.Tracking_Zone import Tracking_Zone  
+from apps.Detection_Zone import Detection_Zone
+from apps.Movement_Zone import Movement_Zone
