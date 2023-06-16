37cebd031e9302785d2011aaaef47b61f5e0753a
MaxChangInnodisk <max_chang@innodisk.com>
2023-06-16 10:40:32 +0800

    feat: remove service file

--- docker/uninstall.sh
+++ docker/uninstall.sh
@@ -5,3 +5,9 @@ SERVICE="ivit-i"
 
 # Disable Service when startup
 sudo systemctl disable ${SERVICE}
+
+# Remove service file
+sudo rm /etc/systemd/system/${SERVICE}.service
+
+# Reload Service
+sudo systemctl daemon-reload
