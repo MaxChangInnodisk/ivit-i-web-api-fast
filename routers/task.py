d517996d42c810978d273eeda975a8e8a7e7c863
MaxChangInnodisk <max_chang@innodisk.com>
2023-08-07 11:58:51 +0800

    feat: check input format is correct or not.

--- routers/task.py
+++ routers/task.py
@@ -187,6 +187,7 @@ def delete_task(del_data: DelTaskFormat):
 def edit_task(edit_data: EditTaskFormat):
     try:
         ret = task_handler.edit_ai_task(edit_data=edit_data)
+        code = 200 if ret['status']=='success' else 500
         return http_msg(content=ret)
     except Exception as e:
         return http_msg(content=e, status_code=500)
