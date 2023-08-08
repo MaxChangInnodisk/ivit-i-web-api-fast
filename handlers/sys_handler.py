1e1b940e18d955f5fbeef03ecb421ddc114a87dc
MaxChangInnodisk <max_chang@innodisk.com>
2023-08-08 14:55:41 +0800

    chore: tidy up code

--- handlers/sys_handler.py
+++ handlers/sys_handler.py
@@ -6,29 +6,30 @@
 import logging as log
 import subprocess as sp
 import cv2
+from typing import Tuple
 
-def get_v4l2() -> tuple:
-    """ Get V4L2 device list [] """
+def get_v4l2() -> Tuple[bool, list]:
+    """Get V4L2 device list
+
+    Returns:
+        Tuple[bool, list]: ( has_camera_or_not, camera_list )
+    """
 
     # Get V4L2 Camera
     command = sp.run("ls /dev/video*", shell=True, stdout=sp.PIPE, encoding='utf8')
     
-    # Not success
+    # Fail mean no camera here, 0 means success
     if command.returncode != 0:
         return ( False, "Camera not found")
 
-    # 0 means success
-
     # Parse Each Camera to a list
     available_camera = []
-
-    camera_list = command.stdout.strip().split('\n')
-    log.debug("{}, {}".format(camera_list, type(camera_list)))
     
     # Check is failed_key in that information
-    for camera in camera_list.copy():
+    for camera in command.stdout.strip().split('\n'):
+        # Note: use `cv2.CAP_V4L` to make sure VideoCapture run fastest.
         cam_idx = int(camera.split("video")[-1])
         if cv2.VideoCapture(cam_idx, cv2.CAP_V4L).isOpened():
             available_camera.append(camera)
         
-    return True, available_camera
+    return ( True, available_camera )
