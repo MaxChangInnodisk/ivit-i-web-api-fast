575d1e2d89772e8a57ad9ca9a49008c103e9c894
MaxChangInnodisk <max_chang@innodisk.com>
2023-08-08 14:38:41 +0800

    feat: change the way to check v4l2 camera

--- handlers/sys_handler.py
+++ handlers/sys_handler.py
@@ -5,30 +5,30 @@
 
 import logging as log
 import subprocess as sp
+import cv2
 
 def get_v4l2() -> tuple:
     """ Get V4L2 device list [] """
-    ret_status, ret_message = True, []
 
     # Get V4L2 Camera
     command = sp.run("ls /dev/video*", shell=True, stdout=sp.PIPE, encoding='utf8')
     
+    # Not success
+    if command.returncode != 0:
+        return ( False, "Camera not found")
+
     # 0 means success
-    if command.returncode == 0:
 
-        # Parse Each Camera to a list
-        ret_message = command.stdout.strip().split('\n')
-        log.debug("{}, {}".format(ret_message, type(ret_message)))
-        
-        # Check is failed_key in that information
-        for msg in ret_message.copy():
-            if int(msg.split("video")[-1])%2==1:
-                # if N is even it's not available for opencv
-                ret_message.remove(msg)
+    # Parse Each Camera to a list
+    available_camera = []
+
+    camera_list = command.stdout.strip().split('\n')
+    log.debug("{}, {}".format(camera_list, type(camera_list)))
     
-    # else not success
-    else:
-        ret_status  = False
-        ret_message = "Camera not found"
-
-    return ret_status, ret_message
+    # Check is failed_key in that information
+    for camera in camera_list.copy():
+        cam_idx = int(camera.split("video")[-1])
+        if cv2.VideoCapture(cam_idx, cv2.CAP_V4L).isOpened():
+            available_camera.append(camera)
+        
+    return True, available_camera
