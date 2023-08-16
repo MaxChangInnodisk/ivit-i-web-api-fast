2708989fa1ffe12002f48c1b0d6241aa26f51b5f
MaxChangInnodisk <max_chang@innodisk.com>
2023-08-16 13:10:25 +0800

    feat: add new api to capture the image via path

--- routers/system.py
+++ routers/system.py
@@ -10,15 +10,18 @@ from fastapi import APIRouter,status
 from fastapi.responses import Response, FileResponse
 from typing import Optional, Dict, List
 from pydantic import BaseModel
+import cv2
 
 try:
     from ..common import SERV_CONF
     from ..handlers import sys_handler
     from ..handlers.mesg_handler import http_msg
+    from .source import frame2buf
 except:
     from common import SERV_CONF
     from handlers import sys_handler
     from handlers.mesg_handler import http_msg
+    from routers.source import frame2buf
 
 # --------------------------------------------------------------------
 # Router
@@ -38,6 +41,7 @@ def get_v4l2_device_list():
 
 @sys_router.get("/files/{file_path}")
 def download_file(file_path):
+    """ For Import export api """
     try:
         file_path = os.path.join( SERV_CONF["EXPORT_DIR"], file_path)
 
@@ -46,6 +50,17 @@ def download_file(file_path):
     except Exception as e:
         return http_msg(content=e, status_code=500)
 
+
+
+@sys_router.get("/images/")
+def get_image(path:str):
+    try:
+        frame = cv2.imread(path)
+        return Response(    
+            content = frame2buf(frame=frame), 
+            status_code = 200, media_type="image/jpeg" )
+    except Exception as e:
+        return http_msg(content=e, status_code=500)
 
 @sys_router.get("/user/permit")
 def change_user_permission():
