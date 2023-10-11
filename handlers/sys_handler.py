640a1c5ca7139ccf3a7958d83c7a264cf8761487
MaxChangInnodisk <max_chang@innodisk.com>
2023-10-11 11:47:51 +0800

    feat: ensure the camera is exist

--- handlers/sys_handler.py
+++ handlers/sys_handler.py
@@ -5,30 +5,52 @@
 
 import logging as log
 import subprocess as sp
+import cv2
+from typing import Tuple
 
-def get_v4l2() -> tuple:
-    """ Get V4L2 device list [] """
-    ret_status, ret_message = True, []
+from .db_handler import (
+    select_data, 
+    parse_source_data,
+    is_list_empty
+)
 
+
+def get_v4l2() -> Tuple[bool, list]:
+    """Get V4L2 device list
+
+    Returns:
+        Tuple[bool, list]: ( has_camera_or_not, camera_list )
+    """
+
+
+    # Get Exist Camera
+    cam_src = []
+    all_cam_src = select_data( table='source', data="*", condition=f"WHERE type='CAM' AND status IN ( 'loaded', 'run' ) " )
+    
+    if not is_list_empty(all_cam_src):        
+        cam_src = [ parse_source_data(cur_src)["input"] for cur_src in all_cam_src ] 
+            
     # Get V4L2 Camera
     command = sp.run("ls /dev/video*", shell=True, stdout=sp.PIPE, encoding='utf8')
     
-    # 0 means success
-    if command.returncode == 0:
+    # Fail mean no camera here, 0 means success
+    if command.returncode != 0:
+        return ( False, "Camera not found")
 
-        # Parse Each Camera to a list
-        ret_message = command.stdout.strip().split('\n')
-        log.debug("{}, {}".format(ret_message, type(ret_message)))
-        
-        # Check is failed_key in that information
-        for msg in ret_message.copy():
-            if int(msg.split("video")[-1])%2==1:
-                # if N is even it's not available for opencv
-                ret_message.remove(msg)
+    # Parse Each Camera to a list
+    available_camera = []
     
-    # else not success
-    else:
-        ret_status  = False
-        ret_message = "Camera not found"
-
-    return ret_status, ret_message
+    # Check is failed_key in that information
+    for camera in command.stdout.strip().split('\n'):
+        
+        cam_idx = int(camera.split("video")[-1])
+        
+        # Note: use `cv2.CAP_V4L` to make sure VideoCapture run fastest.
+        if cv2.VideoCapture(cam_idx, cv2.CAP_V4L).isOpened():
+            available_camera.append(camera)
+        
+        # Note: if the camera source using then append directly
+        elif camera in cam_src:
+            available_camera.append(camera)
+            
+    return ( True, available_camera )
