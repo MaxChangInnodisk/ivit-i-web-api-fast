988ca5f561f71555ff387e7dea66f4d70a4a1636
MaxChangInnodisk <max_change@innodisk.com>
2023-11-28 10:26:25 +0800

    feat: add camera service into launch workflow

--- docker/start_camera.sh
+++ docker/start_camera.sh
@@ -10,6 +10,10 @@ sudo modprobe v4l2loopback devices=2
 gst-launch-1.0 icamerasrc device-name=ev2m_gom1 ! video/x-raw,format=UYVY,width=1920,height=1080 ! v4l2sink device=/dev/video131 &
 gst-launch-1.0 icamerasrc device-name=ev2m_gom1-2 ! video/x-raw,format=UYVY,width=1920,height=1080 ! v4l2sink device=/dev/video132 &
 
+
+sudo i2ctransfer -f -y 3 w3@0x08 0x0A 0x42 0x08
+sudo i2ctransfer -f -y 1 w3@0x08 0x0A 0x42 0x02
+
 # sudo ./end.sh
 
 # gst-launch-1.0 v4l2src device=/dev/video131 ! videoconvert ! videoscale ! video/x-raw,width=960,height=540 ! videomixer name=mix sink_1::xpos=960 ! videoconvert ! fpsdisplaysink video-sink=ximagesink sync=false v4l2src device=/dev/video132 ! videoconvert ! videoscale ! video/x-raw,width=960,height=540 ! mix.
