1b6da15988a68722ca1737f6e15e81f794a95097
MaxChangInnodisk <max_chang@innodisk.com>
2024-06-20 16:25:16 +0800

    fix: remove un-implement function

--- common/__init__.py
+++ common/__init__.py
@@ -9,6 +9,6 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 from common.ivit_socket import manager
 
 from .config import EVENT_CONF, ICAP_CONF, MODEL_CONF, RT_CONF, SERV_CONF, WS_CONF
-from .env import get_ivit_logger_format, init_ivit_env, init_ivit_logger
+from .env import init_ivit_env, init_ivit_logger
 
 init_ivit_env()
