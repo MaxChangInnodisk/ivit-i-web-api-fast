ae726e4f46ce43aeee3835e1e97925f96caf0209
MaxChangInnodisk <max_chang@innodisk.com>
2024-06-20 14:22:14 +0800

    refact: change the ivit_logger usage

--- common/__init__.py
+++ common/__init__.py
@@ -1,25 +1,14 @@
 # Copyright (c) 2023 Innodisk Corporation
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
 
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-from .env import (
-    init_ivit_env,
-    init_ivit_logger
-)
-from .config import (
-    SERV_CONF, 
-    MODEL_CONF, 
-    ICAP_CONF, 
-    RT_CONF, 
-    WS_CONF,
-    EVENT_CONF
-)
+from common.ivit_socket import manager
 
-from .ivit_socket import manager
+from .config import EVENT_CONF, ICAP_CONF, MODEL_CONF, RT_CONF, SERV_CONF, WS_CONF
+from .env import get_ivit_logger_format, init_ivit_env, init_ivit_logger
 
 init_ivit_env()
-init_ivit_logger()
--- handlers/ivit_handler.py
+++ handlers/ivit_handler.py
@@ -3,10 +3,6 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-import common
-
-common.init_ivit_env()
-
 from ivit_i.common import Metric, handle_exception, simple_exception
 from ivit_i.common.app import iAPP_CLS, iAPP_HANDLER, iAPP_OBJ, iAPP_SEG
 from ivit_i.core.models import iModel
